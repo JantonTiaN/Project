4baad5be0da0d32ad449f45f9cdc8696fb59a23b
Janton Wharpheeshin <JantonTiaN@users.noreply.github.com>
2020-04-24 14:44:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:fundee/fund.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
+=======
+import 'package:fundee/home.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+>>>>>>> 6871eee9c71d78d61d80f80a519405a1241c7f5a
 }
 
 class MyApp extends StatelessWidget {
