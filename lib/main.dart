77f1d871ac37bfbd7ad818b9d85acffb6b1f7ec7
bampiyaon <piyaon-p@hotmail.com>
2020-04-24 14:56:01 +0700

    change fund to dentalrecord

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:fundee/fund.dart';
+import 'package:fundee/dentalrecord.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
-=======
-import 'package:fundee/home.dart';
-
-void main() => runApp(MyApp());
-
-
-class MyApp extends StatelessWidget {
-    @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
-  }
->>>>>>> 6871eee9c71d78d61d80f80a519405a1241c7f5a
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FunD(),
+      home: DentalRecord(),
     );
   }
 }
