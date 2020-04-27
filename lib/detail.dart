db220999c83a970090db23dfe4ce494a173212d4
Janton Wharpheeshin <JantonTiaN@users.noreply.github.com>
2020-04-27 15:34:22 +0700

    eiei

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,20 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:fundee/dentalRecord.dart';
 
 class DetailDental extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Dental Charting')),
-      body: Container(
-        child: tooth(),
+      body: Center(
+        child: Text("ไว้ก่อนครับ กำลังจะทำ XD"),
       ),
     );
   }
 }
 
-Widget tooth() {
-  return Container(
-      child: Center(
-          // child: Image.asset('assets/images/1.png'),
-          ));
+
+Widget tooth1Detail(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Tooth1 Detail')),
+    body: Center(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 250),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/1-1.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 75, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/1-2.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 150, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/1-3.png'), 
+              onPressed: null)
+          )
+        ],
+          
+    )
+  ));
+}
+
+Widget tooth2Detail(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Tooth1 Detail')),
+    body: Center(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 250),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/2-1.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 75, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/2-2.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 150, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/2-3.png'), 
+              onPressed: null)
+          )
+        ],
+          
+    )
+  ));
+}
+
+Widget tooth3Detail(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Tooth1 Detail')),
+    body: Center(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 250),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/3-1.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 75, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/3-2.png'), 
+              onPressed: null)
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            margin: EdgeInsets.fromLTRB(0, 150, 0, 0),
+            child: IconButton(
+              icon: Image.asset('images/tooth/detail/3-3.png'), 
+              onPressed: null)
+          )
+        ],
+          
+    )
+  ));
 }
