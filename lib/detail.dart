43c43618a5d90a4ed15381d8d539baed005791a3
Patipat Hanpanboosskorn <35673916+PatipatEarth@users.noreply.github.com>
2020-04-27 17:33:56 +0700

    button

--- lib/detail.dart
+++ lib/detail.dart
@@ -5,16 +5,49 @@ class DetailDental extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Dental Charting')),
-      body: Container(
-        child: tooth(),
+      body: Center(
+        child: Column(
+          children: <Widget>[tooth1(), tooth2(), tooth3()],
+        ),
       ),
+      extendBody: true,
+      bottomNavigationBar: BottomAppBar(
+        clipBehavior: Clip.antiAlias,
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          color: Colors.blue,
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: () {
+              
+            },
+            child: Icon(Icons.add,color: Colors.blue,),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
 
-Widget tooth() {
+Widget tooth1() {
   return Container(
-      child: Center(
-          // child: Image.asset('assets/images/1.png'),
-          ));
+      width: 200,
+      height: 200,
+      child: Image.asset("assets/images/tooth1-1.png"));
+}
+
+Widget tooth2() {
+  return Container(
+      width: 150,
+      height: 150,
+      child: Image.asset("assets/images/tooth1-2.png"));
+}
+
+Widget tooth3() {
+  return Container(
+      width: 200,
+      height: 200,
+      child: Image.asset("assets/images/tooth1-3.png"));
 }
