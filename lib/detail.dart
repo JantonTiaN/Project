d8976641905f724dcb7d8a6343be6c40d4dba22b
Patipat Hanpanboosskorn <35673916+PatipatEarth@users.noreply.github.com>
2020-04-29 15:53:05 +0700

    Update detail.dart

--- lib/detail.dart
+++ lib/detail.dart
@@ -3,30 +3,11 @@ import 'package:fundee/buttonRecord.dart';
 import 'package:fundee/dentalRecord.dart';
 import 'package:fundee/recordData.dart';
 
-class DetailDental extends StatelessWidget {
 class DetailDental extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Dental Charting')),
-      body: Center(
   _DetailDentalState createState() => _DetailDentalState();
 }
 
-      ),
-      extendBody: true,
-      bottomNavigationBar: BottomAppBar(
-        clipBehavior: Clip.antiAlias,
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          color: Colors.blue,
-          height: 50.0,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.white,
-            onPressed: () {
-              
 class _DetailDentalState extends State<DetailDental> {
   bool active = false;
   @override
@@ -46,9 +27,6 @@ class _DetailDentalState extends State<DetailDental> {
                 active = true;
               });
             },
-            child: Icon(Icons.add,color: Colors.blue,),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
           )
         ],
       ),
@@ -67,14 +45,11 @@ class _DetailDentalState extends State<DetailDental> {
     )
     );
   }
-Widget tooth1() {
 }
 
 
   Widget tooth1() {
   return Container(
-      width: 200,
-      height: 200,
       width: 175,
       height: 175,
       child: Image.asset("assets/images/tooth1-1.png"));
@@ -82,8 +57,6 @@ Widget tooth1() {
 
 Widget tooth2() {
   return Container(
-      width: 150,
-      height: 150,
       width: 175,
       height: 175,
       child: Image.asset("assets/images/tooth1-2.png"));
@@ -91,12 +64,9 @@ Widget tooth2() {
 
 Widget tooth3() {
   return Container(
-      width: 200,
-      height: 200,
       width: 175,
       height: 175,
       child: Image.asset("assets/images/tooth1-3.png"));
 
-}
 }
 
