a28d29359321778b39eb07dbf011828f23bf8d49
bampiyaon <piyaon-p@hotmail.com>
2020-05-14 11:28:38 +0700

    มี navigator bar แต่คอมเม้นไว้ก่อน กำลังแก้

--- lib/home.dart
+++ lib/home.dart
@@ -1,40 +1,49 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fundee/dentalRecord.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(appBar: 
-    AppBar(title: Text("Home"),
-    
-    ),
-    body: Container(
-      child: Column(
-        children: <Widget>[
-        FlatButton(
-          child: record(),
-          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DentalRecord()))
-        )
-        ,Text("Dental Record")],
-        
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
       ),
-      
-    ),
-    
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+                child: record(),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DentalRecord()))),
+            Text("Dental Record")
+          ],
+        ),
+      ),
+      // bottomNavigationBar: CurvedNavigationBar(
+        
+      //   items: <Widget>[
+      //     Icon(Icons.home, size: 20, color: Colors.black),
+      //     Icon(Icons.calendar_today, size: 20, color: Colors.black),
+      //     Icon(Icons.add, size: 20, color: Colors.black),
+      //     Icon(Icons.list, size: 20, color: Colors.black),
+      //     Icon(Icons.account_circle, size: 20, color: Colors.black),
+      //   ],
+      //   animationDuration: Duration(milliseconds: 200),
+      //   animationCurve: Curves.bounceInOut,
+      //   onTap: (index) {
+      //     debugPrint("Current Index is $index");
+      //   },
+      // ),
     );
-  
   }
 
-  Widget record(){    
+  Widget record() {
     return Container(
       width: 150.0,
       height: 150.0,
-      child: Image.asset("assets/images/ToothLogo.png"),     
+      child: Image.asset("assets/images/ToothLogo.png"),
     );
-    
-
   }
 }
-
---
+++ lib/patientList.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class DetailDental extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Patients')),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Text("+"),
+    ),
+    );
+  }
+}
