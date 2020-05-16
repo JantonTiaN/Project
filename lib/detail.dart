c3adaf844dff39756152d9b00852dad07d26a123
Patipat Hanpanboosskorn <35673916+PatipatEarth@users.noreply.github.com>
2020-05-16 20:00:29 +0700

    edit Detail

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fundee/dentalCase.dart';
 import 'package:fundee/patientList.dart';
 
 class DetailDental extends StatefulWidget {
@@ -10,286 +11,7 @@ class _DetailDentalState extends State<DetailDental> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tooth Case"),
-      ),
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            FlatButton(              
-              onPressed: (){
-                
-              },
-              child: Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset("assets/images/Dentalcaries.png",height: 75, width: 75,),
-                        Text("Dental Caries", style: TextStyle(fontSize: 25),)
-                      ],
-                    ),
-                  )),
-            ),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Attrition",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Fractured tooth",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Retained root",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "RCT tooth",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Extracted tooth",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Missing tooth",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Impacted tooth",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Partial eruption",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Tilting",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Loss of contact",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Poor contact point",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Food impaction",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Supraclusion",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Infraclusion",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Rotation",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Temporary",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Permanentrestoration",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Gold restoration",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "Porcelain",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      "extract",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            ),
-            Card(
-              color: Colors.blue[100],
-              elevation: 10,
-              child: Container(
-                  height: 100,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Other Detail',),
-                  )),
-            ),
-          ],
-        ),
-      ),
+      
     );
   }
 }
@@ -320,7 +42,7 @@ Widget tooth1Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   ),
                   Container(
@@ -337,7 +59,7 @@ Widget tooth1Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   ),
                   Container(
@@ -354,7 +76,7 @@ Widget tooth1Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   )
                 ],
@@ -385,7 +107,7 @@ Widget tooth2Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) =>DentalCase()));
                         }),
                   ),
                   Container(
@@ -402,7 +124,7 @@ Widget tooth2Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   ),
                   Container(
@@ -419,7 +141,7 @@ Widget tooth2Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   )
                 ],
@@ -450,7 +172,7 @@ Widget tooth3Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   ),
                   Container(
@@ -467,7 +189,7 @@ Widget tooth3Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   ),
                   Container(
@@ -484,7 +206,7 @@ Widget tooth3Detail(BuildContext context) {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => DetailDental()));
+                                  builder: (context) => DentalCase()));
                         }),
                   )
                 ],
