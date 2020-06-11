da3ab08506f2d6272281b255792ea9c3f11eff5c
bampiyaon <piyaon-p@hotmail.com>
2020-06-11 18:17:19 +0700

    create new account form by Bam

--- lib/Screen/signup_screen.dart
+++ lib/Screen/signup_screen.dart
@@ -1,17 +1,123 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 class SignUpScreen extends StatefulWidget {
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
 }
 
+final _formKey = new GlobalKey<FormState>();
+
 class _SignUpScreenState extends State<SignUpScreen> {
+  final fname = TextEditingController();
+  final lname = TextEditingController();
+  final username = TextEditingController();
+  final drugallergy = TextEditingController();
+  final tel = TextEditingController();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final confirmpassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: <Widget>[
           BackButtonWidget(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Icon(
+                    Icons.person,
+                    color: bPrimaryColor,
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(right: 20, left: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: "Firstname"),
+                    controller: fname,
+                  ),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 60, right: 20, bottom: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(right: 20, left: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: "Lastname"),
+                    controller: lname,
+                  ),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Icon(
+                    Icons.mail,
+                    color: bPrimaryColor,
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(right: 20, left: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: "Email Adcress"),
+                    controller: email,
+                  ),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Icon(
+                    Icons.lock,
+                    color: bPrimaryColor,
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(right: 20, left: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: "Password"),
+                    controller: password,
+                  ),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              height: 1.0,
+              // width: 130.0,
+              color: Colors.black12,
+            ),
+          ),
         ],
       ),
     );
@@ -29,57 +135,56 @@ class BackButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 200,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/ToothbrushFixed.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Positioned(
-              child: Stack(
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/ToothbrushFixed.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Positioned(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 20,
+              child: Row(
                 children: <Widget>[
-                  Positioned(
-                    top: 20,
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(
-                            icon: Icon(
-                              Icons.arrow_back_ios,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            }),
-                        InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, 'SignIn');
-                          },
-                          child: Text(
-                            'Back',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        )
-                      ],
+                  IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, 'SignIn');
+                    },
+                    child: Text(
+                      'Back',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Positioned(
-                      bottom: -10,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Create New Account',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                      ))
+                  )
                 ],
               ),
             ),
-          );
+            Positioned(
+                bottom: -10,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Create New Account',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
