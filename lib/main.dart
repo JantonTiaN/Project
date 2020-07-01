7e652b992895a32ce4b231c36961934601396b7b
bampiyaon <piyaon-p@hotmail.com>
2020-07-01 15:02:20 +0700

    change initialroute by Bam

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
           ))),
         ),
         // home: Cupertino(),
-        initialRoute: "DRecord",
+        initialRoute: "OurRoot",
         routes: {
           'SignIn': (context) => SignInScreen(),
           'PSignUp': (context) => PatientSignUpScreen(),
