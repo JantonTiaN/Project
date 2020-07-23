10231573939713672251b7a15f3e9158bb01a678
bampiyaon <piyaon-p@hotmail.com>
2020-07-23 23:48:30 +0700

    done dentist home screen for this release
    เดี๋ยวด้านล่างที่ว่างๆจะทำใน release หน้า เพราะว่าตั้งใจจะเอา appointment มาแสดง

--- lib/Screen/Dentist/dentist_home_screen.dart
+++ lib/Screen/Dentist/dentist_home_screen.dart
@@ -10,7 +10,6 @@ class _DentHomeScreenState extends State<DentHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: bBackgroundColor,
       body: Container(
         // padding: EdgeInsets.symmetric(vertical: 30),
         width: double.infinity,
@@ -22,37 +21,39 @@ class _DentHomeScreenState extends State<DentHomeScreen> {
         ])),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 60),
             Padding(
               padding: EdgeInsets.all(30),
-              child: Row(
+              child: Column(
                 children: <Widget>[
                   Text(
-                    "Hi, ",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    "Welcome, ",
+                    style: TextStyle(color: Colors.white60, fontSize: 16),
+                    textAlign: TextAlign.left,
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(height: 5),
                   Text(
-                    " (user's name)",
+                    "(user's fullname)",
                     style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
                 ],
               ),
             ),
             SizedBox(
-              height: 35,
+              height: 30,
             ),
-            Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50)),
+            Flexible(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)),
+                ),
               ),
-            )),
-            SizedBox()
+            ),
           ],
         ),
       ),
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -14,15 +13,11 @@ class _HomePageState extends State<HomePage> {
         // padding: EdgeInsets.symmetric(vertical: 30),
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-           colors: [
-            Colors.blue[900],
-            Colors.blue[800],
-            Colors.blue[400]
-           ]
-          )
-        ),
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.blue[900],
+          Colors.blue[800],
+          Colors.blue[400]
+        ])),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -30,30 +25,93 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding: EdgeInsets.all(30),
               child: Row(
-                
                 children: <Widget>[
-                  
-                  Text("สวัสดี", style: TextStyle(color: Colors.white, fontSize: 24, fontFamily: 'Kanit'),),
+                  Text(
+                    "สวัสดี",
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 24, fontFamily: 'Kanit'),
+                  ),
                   SizedBox(height: 10),
-                  Text(" วรินทร,", style: TextStyle(color: Colors.white, fontSize: 24, fontFamily: 'Kanit'),),
+                  Text(
+                    " วรินทร,",
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 24, fontFamily: 'Kanit'),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 35,),
+            SizedBox(
+              height: 35,
+            ),
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(50),topRight: Radius.circular(50)),
-                ),
-              )
+                child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
               ),
-              SizedBox(
-
-              )
+            )),
+            SizedBox()
           ],
         ),
       ),
+
+      // body: Container(
+      //   // padding: EdgeInsets.symmetric(vertical: 30),
+      //   width: double.infinity,
+      //   height: 230,
+      //   // height: ,
+      //   decoration: BoxDecoration(
+      //     gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+      //       Colors.blue[800],
+      //       Colors.blue[400],
+      //       Colors.blue[300],
+      //     ]),
+      //     borderRadius: BorderRadius.only(
+      //         bottomLeft: Radius.circular(20),
+      //         bottomRight: Radius.circular(20)),
+      //   ),
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: <Widget>[
+      //       SizedBox(height: 60),
+      //       Padding(
+      //         padding: EdgeInsets.all(30),
+      //         child: Column(
+      //           children: <Widget>[
+      //             Text(
+      //               "Welcome, ",
+      //               style: TextStyle(color: Colors.white60, fontSize: 16),
+      //             ),
+      //             SizedBox(height: 5),
+      //             Text(
+      //               "(user's fullname)",
+      //               style: TextStyle(color: Colors.white, fontSize: 20),
+      //             ),
+      //             Text(
+      //               "Piyaon",
+      //               style: TextStyle(color: Colors.white60, fontSize: 16),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //       // SizedBox(
+      //       //   height: 35,
+      //       // ),
+      //       // Expanded(
+      //       //     child: Container(
+      //       //   decoration: BoxDecoration(
+      //       //     color: Colors.white,
+      //       //     borderRadius: BorderRadius.only(
+      //       //         topLeft: Radius.circular(35),
+      //       //         topRight: Radius.circular(35)),
+      //       //   ),
+      //       // )),
+      //       SizedBox()
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
