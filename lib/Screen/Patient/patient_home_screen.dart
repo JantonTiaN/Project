de7ded12383520ce25a3c994fb6bfef531e02e5b
bampiyaon <piyaon-p@hotmail.com>
2020-08-25 15:06:22 +0700

    temporary patient home

---
+++ lib/Screen/Patient/patient_home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PatientHomeScreen extends StatefulWidget {
+  @override
+  _PatientHomeScreenState createState() => _PatientHomeScreenState();
+}
+
+class _PatientHomeScreenState extends State<PatientHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('This is Patient Home Screen')));
+  }
+}
