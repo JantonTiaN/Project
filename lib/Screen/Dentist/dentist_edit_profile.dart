45384a496fa07f8e0f856119b213c764fd0892ce
bampiyaon <piyaon-p@hotmail.com>
2020-09-21 01:55:25 +0700

    add two function in check button

--- lib/Screen/Dentist/dentist_edit_profile.dart
+++ lib/Screen/Dentist/dentist_edit_profile.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 import 'package:path/path.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -40,112 +41,161 @@ class _DentEditProfileState extends State<DentEditProfile> {
     }
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 300,
-            child: Stack(
-              children: <Widget>[
-                Container(),
-                ClipPath(
-                  clipper: MyCustomClipper(),
-                  child: Container(
-                    height: 260,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            colors: [
-                          Colors.lightBlue[400],
-                          Colors.blue,
-                          Colors.indigo[300]
-                        ])),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              child: Stack(
+                children: <Widget>[
+                  Container(),
+                  ClipPath(
+                    clipper: MyCustomClipper(),
+                    child: Container(
+                      height: 260,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              colors: [
+                            Colors.lightBlue[400],
+                            Colors.blue,
+                            Colors.indigo[300]
+                          ])),
+                    ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment(0, 1),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 65,
-                        backgroundColor: Colors.white,
-                        child: ClipOval(
-                          child: new SizedBox(
-                            width: 120.0,
-                            height: 120.0,
-                            child: (_image != null)
-                                ? Image.file(
-                                    _image,
-                                    fit: BoxFit.fill,
-                                  )
-                                : Image.asset(
-                                    'assets/images/Logo/App-Icon-drop-shadow.jpg',
-                                    fit: BoxFit.fill,
-                                  ),
+                  Align(
+                    alignment: Alignment(0, 1),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 65,
+                          backgroundColor: Colors.white,
+                          child: ClipOval(
+                            child: new SizedBox(
+                              width: 120.0,
+                              height: 120.0,
+                              child: (_image != null)
+                                  ? Image.file(
+                                      _image,
+                                      fit: BoxFit.fill,
+                                    )
+                                  : Image.asset(
+                                      'assets/images/Logo/App-Icon-drop-shadow.jpg',
+                                      fit: BoxFit.fill,
+                                    ),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(widget.user.displayName,
-                          style: TextStyle(
-                              fontSize: 21, fontWeight: FontWeight.bold)),
-                      Text(
-                        'admin@fund.com',
-                        style: TextStyle(fontSize: 14, color: Colors.grey[700]),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 50,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    getImage();
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(210, 210, 0, 0),
-                    child: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.amber[400],
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        LineAwesomeIcons.pen,
-                        color: Colors.black,
-                        size: 20,
+                  GestureDetector(
+                    onTap: () {
+                      getImage();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(210, 210, 0, 0),
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.amber[400],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          LineAwesomeIcons.pen,
+                          color: Colors.black,
+                          size: 20,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SafeArea(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Icon(
-                          Icons.close,
+                  SafeArea(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.close,
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          uploadPic(context);
-                        },
-                        icon: Icon(
-                          Icons.check,
+                        Text(
+                          'Edit Profile',
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                    ],
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            uploadPic(context);
+                          },
+                          icon: Icon(
+                            Icons.check,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
+              color: Colors.red,
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Name',
+                    labelStyle: TextStyle(color: Colors.grey[400])),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Permission',
+                    labelStyle: TextStyle(color: Colors.grey[400])),
+              ),
+            ),
+            Divider(
+              color: Colors.blueGrey[100],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Profile Information',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'E-mail Address',
+                    labelStyle: TextStyle(color: Colors.grey[400]),
+                    hintText: 'Enter your email'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  labelStyle: TextStyle(color: Colors.grey[400]),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
