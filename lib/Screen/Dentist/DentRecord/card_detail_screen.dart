81200632b8e5fce9992bae582164bfe858898b4d
bampiyaon <piyaon-p@hotmail.com>
2020-10-25 20:07:28 +0700

    add card detail screen

---
+++ lib/Screen/Dentist/DentRecord/card_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CardDetailScreen extends StatefulWidget {
+  @override
+  _CardDetailScreenState createState() => _CardDetailScreenState();
+}
+
+class _CardDetailScreenState extends State<CardDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Case Detail'),
+      ),
+      body: Column(),
+    );
+  }
+}
