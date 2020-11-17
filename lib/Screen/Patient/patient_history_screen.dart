7316c4ba4ea9df4ff85c9ab1b47b20f626216922
bampiyaon <piyaon-p@hotmail.com>
2020-11-18 01:50:26 +0700

    patienth his error

--- lib/Screen/Patient/patient_history_screen.dart
+++ lib/Screen/Patient/patient_history_screen.dart
@@ -2,8 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:fundee/Screen/Patient/patient_menu_screen.dart';
-import 'package:fundee/Screen/Patient/suggestion_detail_screen.dart';
+import 'package:fundee/Screen/Dentist/dentist_menu_screen.dart';
 
 class PatientHistoryScreen extends StatefulWidget {
   final FirebaseUser user;
@@ -13,6 +12,8 @@ class PatientHistoryScreen extends StatefulWidget {
 }
 
 class _PatientHistoryScreenState extends State<PatientHistoryScreen> {
+  List alldata = [];
+
   @override
   void initState() {
     super.initState();
@@ -21,124 +22,267 @@ class _PatientHistoryScreenState extends State<PatientHistoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Your History Record'),
-        backgroundColor: Colors.blue[300],
-        automaticallyImplyLeading: false,
-      ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('FunD')
-            .document('funD')
-            .collection('Clinic')
-            .document('clinic')
-            .collection(clinic)
-            .document(clinic)
-            .collection('Patients')
-            .document(widget.user.uid)
-            .collection('History')
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SpinKitChasingDots(
-                    color: Colors.blue[100],
-                    size: 50,
-                  ),
-                  Text(
-                    'Loading...',
-                    style: TextStyle(fontSize: 15, color: Colors.black),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('History'),
+              backgroundColor: Colors.blue[300],
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(text: 'On Progress'),
+                  Tab(
+                    text: 'Finished',
                   )
                 ],
-              ),
-            );
-          } else if (snapshot.data.documents[0].data.length == 0) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/Logo/No-data.png',
-                    width: 150,
-                    height: 150,
-                  ),
-                  Text(
-                    'Oh...',
-                    style: TextStyle(
-                        fontFamily: 'Kanit',
-                        color: Colors.blue[300],
-                        fontSize: 25),
-                  ),
-                  Text(
-                    'You don\'t have any history',
-                    style: TextStyle(
-                        fontFamily: 'Kanit',
-                        color: Colors.blue[300],
-                        fontSize: 16),
-                  ),
-                ],
-              ),
-            );
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data.documents[0].data['history'].length,
-              itemBuilder: (context, index) {
-                final idx = index + 1;
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          '   $idx ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.blue),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Case: ' +
-                                  snapshot.data.documents[0].data['history']
-                                      [index]['Detail']),
-                              Text('Date: ' +
-                                  snapshot.data.documents[0].data['history']
-                                      [index]['Date']),
-                              Text(
-                                'Status: ' +
-                                    snapshot.data.documents[0].data['history']
-                                        [index]['Status'],
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue),
-                              )
-                            ],
+                indicatorColor: Colors.white,
+                indicatorWeight: 4,
+                labelColor: Colors.white,
+                // unselectedLabelColor: Colors.black,
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+              )),
+          body: TabBarView(
+            children: [
+              StreamBuilder(
+                stream: Firestore.instance
+                    .collection('FunD')
+                    .document('funD')
+                    .collection('Clinic')
+                    .document('clinic')
+                    .collection(clinic)
+                    .document(clinic)
+                    .collection('Patients')
+                    .document(uid)
+                    .collection('History')
+                    .document('history')
+                    .collection('OnProgress')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SpinKitChasingDots(
+                            color: Colors.blue[100],
+                            size: 50,
                           ),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Text('Responsible by: ' +
-                          snapshot.data.documents[0].data['history'][index]
-                              ['Dentist']),
-                    ),
-                    Divider(
-                      color: Colors.blueGrey,
-                    )
-                  ],
-                );
-              },
-            );
-          }
-        },
-      ),
+                          Text(
+                            'Loading...',
+                            style: TextStyle(fontSize: 15, color: Colors.black),
+                          )
+                        ],
+                      ),
+                    );
+                  } else if (snapshot.data.documents[0].data.isEmpty) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '0 history record(s) found',
+                            style: TextStyle(
+                                fontFamily: 'Kanit',
+                                color: Colors.blue[300],
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else if (snapshot.data.documents[0].data.isNotEmpty) {
+                    if (snapshot.data.documents[0].data['history'].length ==
+                        0) {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              '0 history record(s) found',
+                              style: TextStyle(
+                                  fontFamily: 'Kanit',
+                                  color: Colors.blue[300],
+                                  fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount:
+                            snapshot.data.documents[0].data['history'].length.,
+                        itemBuilder: (context, index) {
+                          final idx = index + 1;
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      '   $idx ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text('Case: ' +
+                                              snapshot.data.documents[0]
+                                                      .data['history'][index]
+                                                  ['Detail']),
+                                          Text('Date: ' +
+                                              snapshot.data.documents[0]
+                                                      .data['history'][index]
+                                                  ['Date']),
+                                          Text('Dentist: ' +
+                                              snapshot.data.documents[0]
+                                                      .data['history'][index]
+                                                  ['Dentist']),
+                                          Text(
+                                            'Status: ' +
+                                                snapshot.data.documents[0]
+                                                        .data['history'][index]
+                                                    ['Status'],
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.blue),
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  }
+                },
+              ),
+              StreamBuilder(
+                stream: Firestore.instance
+                    .collection('FunD')
+                    .document('funD')
+                    .collection('Clinic')
+                    .document('clinic')
+                    .collection(clinic)
+                    .document(clinic)
+                    .collection('Patients')
+                    .document(uid)
+                    .collection('History')
+                    .document('history')
+                    .collection('Finished')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SpinKitChasingDots(
+                            color: Colors.blue[100],
+                            size: 50,
+                          ),
+                          Text(
+                            'Loading...',
+                            style: TextStyle(fontSize: 15, color: Colors.black),
+                          )
+                        ],
+                      ),
+                    );
+                  } else if (snapshot.data.documents[0].data.length == 0) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '0 history record(s) found',
+                            style: TextStyle(
+                                fontFamily: 'Kanit',
+                                color: Colors.blue[300],
+                                fontSize: 16),
+                          ),
+                          // )
+                        ],
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount:
+                          snapshot.data.documents[0].data['history'].length,
+                      itemBuilder: (context, index) {
+                        final idx = index + 1;
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: Container(
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    '   $idx ',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Case: ' +
+                                            snapshot.data.documents[0]
+                                                    .data['history'][index]
+                                                ['Detail']),
+                                        Text('Date: ' +
+                                            snapshot.data.documents[0]
+                                                    .data['history'][index]
+                                                ['Date']),
+                                        Text('Dentist: ' +
+                                            snapshot.data.documents[0]
+                                                    .data['history'][index]
+                                                ['Dentist']),
+                                        Text(
+                                          'Status: ' +
+                                              snapshot.data.documents[0]
+                                                      .data['history'][index]
+                                                  ['Status'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.blue),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
+          )),
     );
   }
+}
+
+String uid;
+void getuserid(String getuserid) {
+  uid = getuserid;
 }
